2ccfa354bf507e6a8ca4c024efdb147c4b065a99
PurpleLightSpeed <lotaagulue@gmail.com>
2025-06-13 10:04:43 -0400

    Uodated Sneaker Game

--- click_the_sneaker_game.py
+++ click_the_sneaker_game.py
@@ -6,8 +6,8 @@ WINDOW_WIDTH = 500
 WINDOW_HEIGHT = 400
 
 def move_sneaker():
-    x = random.radiant(0, WINDOW_WIDTH - 100)
-    y = random.radiant(0, WINDOW_HEIGHT - 100)
+    x = random.randint(0, WINDOW_WIDTH - 100)
+    y = random.randint(0, WINDOW_HEIGHT - 100)
     sneaker_button.place(x=x, y=y)
 
 def sneaker_clicked():
@@ -19,3 +19,14 @@ def sneaker_clicked():
 # Initialize the main application window
 root = tk.Tk()
 root.title("Click the Sneaker Game")
+root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
+
+score = 0
+score_label = tk.Label(root, text = "score: 0", font = ("Arial", 16))
+score_label.pack(pady=10)
+
+# Create a button that represents the sneaker
+sneaker_button = tk.Button(root, text="👟", font=("Arial", 50), command=sneaker_clicked, bd = 0)
+move_sneaker()  # Position the sneaker button randomly
+
+root.mainloop()  # Start the main event loop
